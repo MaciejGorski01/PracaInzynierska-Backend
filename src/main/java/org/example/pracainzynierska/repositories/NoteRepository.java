b11bdc685132753fbf792cf5e55bfaf2409468c0
Jigokuzuki <maciek.gorski888@wp.pl>
2024-09-04 12:46:58 +0200

    Added all needed queries

--- src/main/java/org/example/pracainzynierska/repositories/NoteRepository.java
+++ src/main/java/org/example/pracainzynierska/repositories/NoteRepository.java
@@ -8,12 +8,16 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface NoteRepository extends CrudRepository <Note, Long> {
 
     @Query("SELECT n from Note n WHERE n.note_owner_id = :userid")
     List<Note> findAllUserNotes(@Param("userid") Long userid);
+
+    @Query("SELECT n from Note n WHERE n.id = :id")
+    Optional<Note> findById(@Param("id") Long id);
 
     @Modifying
     @Query(value = "INSERT INTO Note n (n.title, n.tag, n.favourite, n.content, n.imageUrl, n.fileUrl, n.note_owner_id) VALUES (:title, :tag, :favourite, :content, :imageUrl, :fileUrl, :note_owner_id);", nativeQuery = true)
@@ -29,5 +33,15 @@ public interface NoteRepository extends CrudRepository <Note, Long> {
     @Modifying
     @Query("DELETE FROM Note n WHERE n.id = :id")
     void delete(@Param("id") Long id);
+
+    @Modifying
+    @Query("UPDATE Note n SET n.title = :title, n.tag = :tag, n.favourite = :favourite, n.content = :content, n.imageUrl = :imageUrl, n.fileUrl = :fileUrl WHERE n.id = :id")
+    void update(@Param("title") String title,
+                @Param("tag") String tag,
+                @Param("favourite") boolean favourite,
+                @Param("content") String content,
+                @Param("imageUrl") String imageUrl,
+                @Param("fileUrl") String fileUrl,
+                @Param("id") Long id);
 
 }
