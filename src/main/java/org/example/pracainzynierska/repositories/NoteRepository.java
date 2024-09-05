949ad741c071e490d46d7b09c5d26816065594f3
Jigokuzuki <maciek.gorski888@wp.pl>
2024-09-05 15:12:55 +0200

    Added mapping and jdbcTemplate

--- src/main/java/org/example/pracainzynierska/repositories/NoteRepository.java
+++ src/main/java/org/example/pracainzynierska/repositories/NoteRepository.java
@@ -1,47 +1,43 @@
 package org.example.pracainzynierska.repositories;
 
+import org.example.pracainzynierska.mappers.NoteMapper;
 import org.example.pracainzynierska.models.Note;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
-public interface NoteRepository extends CrudRepository <Note, Long> {
+public class NoteRepository {
 
-    @Query("SELECT n from Note n WHERE n.note_owner_id = :userid")
-    List<Note> findAllUserNotes(@Param("userid") Long userid);
+    private final JdbcTemplate jdbcTemplate;
 
-    @Query("SELECT n from Note n WHERE n.id = :id")
-    Optional<Note> findById(@Param("id") Long id);
+    public NoteRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
-    @Modifying
-    @Query(value = "INSERT INTO Note n (n.title, n.tag, n.favourite, n.content, n.imageUrl, n.fileUrl, n.note_owner_id) VALUES (:title, :tag, :favourite, :content, :imageUrl, :fileUrl, :note_owner_id);", nativeQuery = true)
-    void create(@Param("title") String title,
-                @Param("tag") String tag,
-                @Param("favourite") Boolean favourite,
-                @Param("content") String content,
-                @Param("imageUrl") String imageUrl,
-                @Param("fileUrl") String fileUrl,
-                @Param("note_owner_id") Long note_owner_id);
+    public List<Note> findAllUserNotes(Long userid){
+        String sql = "SELECT n from Note n WHERE n.note_owner_id = ?";
+        return jdbcTemplate.query(sql, new Object[]{userid}, new NoteMapper());
+    }
 
+    public Note findById(Long id){
+        String sql = "SELECT n from Note n WHERE n.id = ?";
+        return jdbcTemplate.queryForObject(sql, new Object[]{id}, new NoteMapper());
+    }
 
-    @Modifying
-    @Query("DELETE FROM Note n WHERE n.id = :id")
-    void delete(@Param("id") Long id);
+    public void create(String title, String tag, Boolean favourite, String content, String imageUrl, String fileUrl, Long note_owner_id){
+        String sql = "INSERT INTO Note n (n.title, n.tag, n.favourite, n.content, n.imageUrl, n.fileUrl, n.note_owner_id) VALUES (?, ?, ?, ?, ?, ?, ?);";
+        jdbcTemplate.update(sql, title, tag, favourite, content, imageUrl, fileUrl, note_owner_id);
+    }
 
-    @Modifying
-    @Query("UPDATE Note n SET n.title = :title, n.tag = :tag, n.favourite = :favourite, n.content = :content, n.imageUrl = :imageUrl, n.fileUrl = :fileUrl WHERE n.id = :id")
-    void update(@Param("title") String title,
-                @Param("tag") String tag,
-                @Param("favourite") Boolean favourite,
-                @Param("content") String content,
-                @Param("imageUrl") String imageUrl,
-                @Param("fileUrl") String fileUrl,
-                @Param("id") Long id);
+    public void delete(Long id){
+        String sql = "DELETE FROM Note n WHERE n.id = :id";
+        jdbcTemplate.update(sql, id);
+    }
 
+    public void update(String title, String tag, Boolean favourite, String content, String imageUrl, String fileUrl, Long id){
+        String sql = "UPDATE Note n SET n.title = ? n.tag = ?, n.favourite = ?, n.content = ?, n.imageUrl = ?, n.fileUrl = ? WHERE n.id = ?";
+        jdbcTemplate.update(sql, title, tag, favourite, content, imageUrl, fileUrl, id);
+    }
 }
--- src/main/java/org/example/pracainzynierska/repositories/UserRepository.java
+++ src/main/java/org/example/pracainzynierska/repositories/UserRepository.java
@@ -1,45 +1,54 @@
 package org.example.pracainzynierska.repositories;
 
+import org.example.pracainzynierska.mappers.UserMapper;
 import org.example.pracainzynierska.models.User;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public class UserRepository {
 
-    @Query("SELECT u from User u")
-    List<User> findAll();
+    private final JdbcTemplate jdbcTemplate;
 
-    @Query("SELECT u FROM User u WHERE u.id = :id")
-    Optional<User> findById(@Param("id") Long id);
+    public UserRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
-    @Modifying
-    @Query(value = "INSERT INTO User u (u.password, u.email, u.name, u.surname) VALUES (:password, :email, :name, :surname);", nativeQuery = true)
-    User create(@Param("password") String password,
-                @Param("email") String email,
-                @Param("name") String name,
-                @Param("surname") String surname);
+    public List<User> findAll(){
+        String sql = "SELECT u from User u";
+        return jdbcTemplate.query(sql, new UserMapper());
+    }
 
-    @Query("SELECT u FROM User u WHERE u.email = :email")
-    User findUserByEmail(@Param("email") String email);
+    public User findById(Long id){
+        String sql = "SELECT u FROM User u WHERE u.id = ?";
+        return jdbcTemplate.queryForObject(sql, new Object[] {id}, new UserMapper());
+    }
 
-    @Modifying
-    @Query("UPDATE User u SET u.name = :name, u.surname = :surname, u.password = :password WHERE u.id = :id AND u.email = :email ")
-    void update(@Param("password") String password,
-                @Param("email") String email,
-                @Param("name") String name,
-                @Param("surname") String surname,
-                @Param("id") Long id);
+    public void create(String password, String email, String name, String surname){
+        String sql = "INSERT INTO User u (u.password, u.email, u.name, u.surname) VALUES (?, ?, ?, ?)";
+        jdbcTemplate.update(sql, password, email, name, surname);
+    }
 
-    @Modifying
-    @Query("DELETE FROM User u WHERE u.id = :id")
-    void delete(@Param("id") Long id);
+    public User findUserByEmail(String email){
+        String sql = "SELECT u FROM User u WHERE u.email = ?";
+        return jdbcTemplate.queryForObject(sql, new Object[]{email}, new UserMapper());
+    }
 
+    public void update(String password, String email, String name, String surname, Long id){
+        String sql = "UPDATE User u SET u.name = :name, u.surname = :surname, u.password = :password WHERE u.id = :id";
+        jdbcTemplate.update(sql, password, email, name, surname, id);
+    }
+
+    public void delete(Long id){
+        String sql = "DELETE FROM User u WHERE u.id = :id";
+        jdbcTemplate.update(sql, id);
+    }
 
 }
