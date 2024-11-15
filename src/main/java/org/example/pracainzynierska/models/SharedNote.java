d8e32d4ce7e438bb42ab016ffb89d5e3f5f13f8e
Jigokuzuki <maciek.gorski888@wp.pl>
2024-11-15 21:59:52 +0100

    Added SharedNote model

---
+++ src/main/java/org/example/pracainzynierska/models/SharedNote.java
@@ -0,0 +1,17 @@
+package org.example.pracainzynierska.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class SharedNote {
+
+    String id;
+
+    String note_id;
+
+    String shared_with_user_id;
+
+}
